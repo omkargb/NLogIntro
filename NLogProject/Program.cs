553582ef1096d336b7d215ff6b98166e2dc658e8
omkargb <omkargb107@gmail.com>
2021-07-29 11:16:26 +0530

    [omkargb] Add : Logging using Nlog implemented

---
+++ NLogProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to first NLog project.");
+            addition ad = new addition();
+            ad.Sum(50,100);
+        }
+    }
+}
---
+++ NLogProject/addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogProject
+{
+    class addition
+    {
+        private static readonly NLog nLog = new NLog();
+        public int Sum(int num1, int num2)
+        {
+            if(num1==0 || num2==0)
+            {
+                nLog.LogDebug(" Debug Success. : Sum() ");
+                nLog.LogError(" Expecting null values");
+                nLog.LogWarn(" First number or Second number should not be equal.");
+            }
+            int answer = num1 + num2;
+            nLog.LogDebug(" Debug success. : Sum() ");
+            nLog.LogInfo(" Sum method passed. Result : " + answer);
+            Console.WriteLine(" Sum : " + answer);
+            return answer;
+        }
+    }
+}
